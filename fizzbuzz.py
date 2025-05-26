cde348f2a64a08eac250f8044f0e39303af37572
FernandoBlanco10 <blanco.alvarez.moy2002@gmail.com>
2025-05-26 13:15:44 -0600

    Modificacion de orden

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for num in range(1,1000):
-    if num % 3 == 0:
+for num in range(1,100):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 3 == 0:
         print("fizz")
     elif num % 5 == 0:
         print("buzz")
-    elif num % 3 == 0 and num % 5 == 0:
-        print("fizzbuzz")
     else:
         print(num)
